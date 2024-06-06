ce59f5420bec2bebe6ff353d4284ff7e3cfcd3c0
Anshul-Sangrame <anshul.sangrame@gmail.com>
2024-06-06 13:49:34 +0530

    update serviceListDisplayer

--- lib/components/service_displayer.dart
+++ lib/components/service_displayer.dart
@@ -3,15 +3,23 @@ import 'package:flutter/material.dart';
 class ServiceCard extends StatelessWidget {
   const ServiceCard({
     super.key,
-    required this.temp,
+    required this.imageURL,
   });
 
-  final int temp;
+  final String imageURL;
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Text("This is a card $temp"),
+    return Container(
+      padding: EdgeInsets.all(10),
+      constraints: BoxConstraints.expand(height: 200),
+      child: Ink.image(
+        image: NetworkImage(imageURL),
+        fit: BoxFit.fill,
+        child: InkWell(
+          onTap: () {},
+        ),
+      ),
     );
   }
 }
@@ -19,15 +27,23 @@ class ServiceCard extends StatelessWidget {
 class ServiceCardList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var listOfCard = <Widget>[];
-    for(int i =0 ;i <1000;i++)
-    {
-      listOfCard.add(ServiceCard(temp: i));
-    }
     return Container(
       padding: EdgeInsets.all(5),
       child: ListView(
-        children: listOfCard,
+        children: [
+          ServiceCard(
+            imageURL:
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJjUzSjsGfs4CVQ4dgvNdtOkS4-wbb4vWPXA&s",
+          ),
+          ServiceCard(
+            imageURL:
+                "https://static.vecteezy.com/system/resources/previews/017/462/988/large_2x/catering-services-background-with-snacks-and-glasses-on-bartender-counter-in-restaurant-photo.jpg",
+          ),
+          ServiceCard(
+            imageURL:
+                "https://5.imimg.com/data5/SELLER/Default/2023/10/351027020/WQ/VY/LA/10351111/hotel-accommodation-service.jpeg",
+          ),
+        ],
       ),
     );
   }
