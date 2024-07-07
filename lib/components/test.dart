4255df18ffe02ad89294b2d7f6bb43656058ff4c
Anshul-Sangrame <anshul.sangrame@gmail.com>
2024-07-07 16:47:54 +0530

    fixed minor warning

--- lib/components/test.dart
+++ lib/components/test.dart
@@ -1,26 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:startup/components/service_displayer.dart';
-import '../states/state.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:startup/components/service_displayer.dart';
+// import '../states/state.dart';
 
 class Test extends StatelessWidget { 
   @override 
   Widget build(BuildContext context) { 
     double height = 100;
-    return Container( 
-          child: Center( 
-            child: Container(
-              height: height,
-              width: 100,
-              child: LayoutBuilder( 
-                builder: (BuildContext context, BoxConstraints constraints) { 
-                  height = constraints.maxHeight; 
-                
-                  return Text("The height of the container is $height"); 
-                }, 
-              ),
-            ), 
-          ), 
-        ); 
+    return Center( 
+      child: SizedBox(
+        height: height,
+        width: 100,
+        child: LayoutBuilder( 
+          builder: (BuildContext context, BoxConstraints constraints) { 
+            height = constraints.maxHeight; 
+          
+            return Text("The height of the container is $height"); 
+          }, 
+        ),
+      ), 
+    ); 
   } 
 } 
